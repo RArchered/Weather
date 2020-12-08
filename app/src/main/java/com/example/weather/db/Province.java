9dadd139618a708cc269b9bb50bff0937a05eaee
Archer <2776130131@qq.com>
2020-12-08 11:23:27 +0800

    add province

---
+++ app/src/main/java/com/example/weather/db/Province.java
@@ -0,0 +1,33 @@
+package com.example.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
