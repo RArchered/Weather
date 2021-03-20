23cc48a7710a8dbf97adac8cad87c7056de12ef6
Archer <2776130131@qq.com>
2021-03-20 23:32:50 +0800

    change to mvp

--- app/src/main/java/com/example/weather/startpage/StartPageActivity.java
+++ app/src/main/java/com/example/weather/startpage/StartPageActivity.java
@@ -1,6 +1,32 @@
 package com.example.weather.startpage;
 
-import com.example.weather.BasePresenter;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class StartPageActivity implements BasePresenter {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.weather.BasePresenter;
+import com.example.weather.R;
+import com.example.weather.WeatherActivity;
+
+public class StartPageActivity extends AppCompatActivity implements BasePresenter {
+
+    private long mExitTime = System.currentTimeMillis() - 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("TAG", "Hello, git!");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 }
